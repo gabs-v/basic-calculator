c0dd1987f999bfe25455678e6aa9ba6f78e367f6
gabs-v <gabyvazquez8297@gmail.com>
2022-03-19 16:18:17 -0700

    first commit

---
+++ basic_addition_calculator.py
@@ -0,0 +1,7 @@
+#A very simple addition calculator that gets me familiar with getting input from the user! :) 
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2) 
+
+print(result)
